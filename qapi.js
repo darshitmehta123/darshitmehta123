608ddb8cc8b22e25a3bf8b2631ffb9947dc48336
darshitmehta123 <75662148+darshitmehta123@users.noreply.github.com>
2020-12-08 12:38:20 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcf2186e563f3001ecb7e6c",
+				"5fcf2186e563f3001ecb7e6e"
+			]
+		},
+		"5fcf2186e563f3001ecb7e6c": {
+			"id": "5fcf2186e563f3001ecb7e6c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcf2186e563f3001ecb7e6e": {
+			"id": "5fcf2186e563f3001ecb7e6e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
